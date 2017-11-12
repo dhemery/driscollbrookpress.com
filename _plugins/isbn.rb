6467c6fa3386373be9235cfecf21ee2e438d804b
Dale Emery <dale@dhemery.com>
2017-11-11 16:00:41 -0800

    Clean up ISBN calculation code

--- _plugins/isbn.rb
+++ _plugins/isbn.rb
@@ -3,37 +3,38 @@ module ISBN
   PUBLISHER_GROUP = '1'
   DRISCOLL_BROOK_PRESS = '63261'
 
+  ISBN10_FORMULA = {
+      prefix: [PUBLISHER_GROUP, DRISCOLL_BROOK_PRESS],
+      checksum_multipliers: [10, 9, 8, 7, 6, 5, 4, 3, 2],
+      check_digit: lambda do |checksum|
+        check_digit = (11 - (checksum % 11)) % 11
+        check_digit == 10 ? 'X' : check_digit
+      end
+  }
+  ISBN13_FORMULA = {
+      prefix: [BOOKLAND] + ISBN10_FORMULA[:prefix],
+      checksum_multipliers: [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3],
+      check_digit: lambda do |checksum|
+        (10 - (checksum % 10)) % 10
+      end
+  }
+
   def isbn10(title, separator = '')
-    identifier = '%03d' % title.to_i
-    isbn_parts = [PUBLISHER_GROUP, DRISCOLL_BROOK_PRESS, identifier]
-    multiplier = 10
-    checksum = 0
-    isbn_parts.join.each_char do |c|
-      digit = c.to_i
-      term = digit * multiplier
-      checksum += term
-      multiplier -= 1
-    end
-    check_digit = (11 - (checksum % 11)) % 11
-    check_digit = 'X' if check_digit == 10
-    isbn_parts <<= check_digit.to_s
-    isbn_parts.join(separator)
+    isbn(ISBN10_FORMULA, title, separator)
   end
 
   def isbn13(title, separator = '')
-    identifier = '%03d' % title.to_i
-    isbn_parts = [BOOKLAND, PUBLISHER_GROUP, DRISCOLL_BROOK_PRESS, identifier]
-    multiplier = 1
-    checksum = 0
-    isbn_parts.join.each_char do |c|
-      digit = c.to_i
-      term = digit * multiplier
-      checksum += term
-      multiplier = (multiplier + 2) % 4
-    end
+    isbn(ISBN13_FORMULA, title, separator)
+  end
 
-    check_digit = (10 - (checksum % 10)) % 10
-    isbn_parts <<= check_digit.to_s
+  def isbn(formula, title, separator)
+    isbn_parts = formula[:prefix] + ['%03d' % title]
+    checksum = isbn_parts.join.chars.map(&:to_i)
+                   .zip(formula[:checksum_multipliers])
+                   .map {|digit, multiplier| digit * multiplier}
+                   .reduce(&:+)
+    check_digit = formula[:check_digit].call(checksum).to_s
+    isbn_parts <<= check_digit
     isbn_parts.join(separator)
   end
 
